b37566cf553cfd5835246c574e44d6fe6f9326d0
SaidkhojaIftikhor <saidkhojaiftikhor2002@gmail.com>
2024-02-29 22:26:55 +0500

    add husky and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import {QueryClient, QueryClientProvider, useQueryClient} from "@tanstack/react-query";
-import {Outlet} from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
-        retry: false
-      }
-    }
+        retry: false,
+      },
+    },
   });
 
   return (
-      <QueryClientProvider client={queryClient}>
-        <Outlet />
-      </QueryClientProvider>
-  )
+    <QueryClientProvider client={queryClient}>
+      <Outlet />
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
